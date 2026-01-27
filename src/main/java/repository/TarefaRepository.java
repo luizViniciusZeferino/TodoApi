e2dc7b8829af819988db5c5c9407ea9576e6c3aa
LuizViniciusZeferino <luizvinicius1530@gmail.com>
2026-01-27 20:53:32 -0300

    Cria repository e entity

---
+++ src/main/java/repository/TarefaRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import entity.TarefaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface TarefaRepository extends JpaRepository<TarefaEntity, Long> { // Repositorio gerencia entidade TarefaEntity que tem id tipo Long
+}
